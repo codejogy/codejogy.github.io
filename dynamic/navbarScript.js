5ac629aedb5f95c542871b37fc1be13e9d31546e
Jose Hernandez <josehdzibarra@hotmail.com>
2026-01-07 02:11:40 -0600

    Added Saber Mas, buttons need to work still

--- dynamic/navbarScript.js
+++ dynamic/navbarScript.js
@@ -2,13 +2,26 @@
 
 let checkMozilla = navigator.userAgent.includes("Mozilla"); // boolean
 
+// Add navbar
+const nav = document.querySelector(".navbar");
+fetch("../views/navbar.html")
+    .then(res => res.text())
+    .then(data => { nav.innerHTML = data })
+
+// Add footer
+const foot = document.querySelector(".footer");
+fetch("../views/footer.html")
+    .then(res => res.text())
+    .then(data => { foot.innerHTML = data })
+
+// If using mozilla
 if (checkMozilla) {
-	document.querySelector(".navbar").setAttribute("style", "background-color:#e3e3e378");
+    document.querySelector(".navbar").setAttribute("style", "background-color:#e3e3e378");
 
 }
 
 
-console.log(checkMozilla);
-console.debug(navigator.userAgent);
-console.debug("hello");
+//console.log(checkMozilla);
+//console.debug(navigator.userAgent);
+//console.debug("hello");
 
